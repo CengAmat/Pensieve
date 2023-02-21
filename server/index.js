53a8472671a419a7c0be58e39dc246297b05d123
cengamat <azurnaci@cultwinesltd.com>
2023-02-21 18:34:31 +0000

    Connecting to the Database

--- server/index.js
+++ server/index.js
@@ -8,3 +8,22 @@ const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://cengamat:cengamat@cluster-2g47mmc1.sridj.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5001;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+// mongoose.set("useFindAndModify", false);
+mongoose.set("strictQuery", false);
+
+// https://www.mongodb.com/cloud/atlas
