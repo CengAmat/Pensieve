84efb2dcb7e63fde233cd89850b09744ff37a6ee
cengamat <azurnaci@cultwinesltd.com>
2023-02-28 15:18:10 +0000

    Styles added to ts components

---
+++ client-ts/src/components/Form/styles.tsx
@@ -0,0 +1,24 @@
+import { makeStyles } from "@mui/material";
+
+export default makeStyles((theme: any) => ({
+  root: {
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+    },
+  },
+  paper: {
+    padding: theme.spacing(2),
+  },
+  form: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+  },
+  fileInput: {
+    width: "97%",
+    margin: "10px 0",
+  },
+  buttonSubmit: {
+    marginBottom: 10,
+  },
+}));
---
+++ client-ts/src/components/Posts/Post/Post.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import useStyles from "./styles";
+
+const Post: React.FC = () => {
+  const classes = useStyles();
+
+  return <div>Post</div>;
+};
+
+export default Post;
